2539f1a869a205d7104b66540eeb5fa13d603d7b
ghost2023 <nathnaelwonisha@gmail.com>
2024-05-17 01:01:57 +0300

    add colors to tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,12 @@ const config = {
       },
     },
     extend: {
+    colors: {
+      primary: "#006769",
+      accent: '#40A578',
+        lime: '#9DDE8B',
+        lemon: '#E6FF94'
+    },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -35,6 +41,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
