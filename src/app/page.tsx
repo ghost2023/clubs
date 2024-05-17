e7f6a320d7514e7ef3c7cefb320f1a82e8ac9101
ghost2023 <nathnaelwonisha@gmail.com>
2024-05-17 03:34:20 +0300

    build home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,129 @@
-import Link from "next/link";
+import { Club, FundRaise, Post } from "@prisma/client"
+import MainLayout from "~/components/MainLayout"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import { getServerAuthSession } from "~/server/auth"
+import { db } from "~/server/db"
+import DonateDailog from "./DonateDailog"
 
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+export default async function Component() {
+  const session = await getServerAuthSession()
+  const posts = await db.post.findMany({
+    include: {
+      club: true
+    }
+  })
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
+  const fundraising = await db.fundRaise.findMany({
+    include: {
+      club: true,
+    },
+  })
 
+  const clubs = await db.club.findMany()
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+    <MainLayout>
 
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
+      <div className="flex gap-3 bg-gray-100 ">
+        <div className="flex flex-col py-8 gap-4 overflow-auto px-7">
+          {fundraising.map(post => <FundCard fund={post}/> )}
+          {posts.map(post => <PostCard post={post}/> )}
         </div>
 
-        <CrudShowcase />
+        <div className="flex-col py-8 gap-4 overflow-auto hidden sm:flex">
+        <div className="font-medium">Suggested for you</div>
+        <div className="flex flex-col gap-4">
+            {clubs.map(club => <ClubCard club={club} />) }
+        </div>
       </div>
-    </main>
-  );
+      </div>
+    </MainLayout> 
+  )
 }
 
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
+const ClubCard = ({club}: {club: Club}) => {
+         return <div className="flex items-center gap-2">
+            <Avatar className="w-8 h-8">
+              <AvatarImage alt="@shadcn" src={'/uploads/' + club.image}/>
+              <AvatarFallback>JP</AvatarFallback>
+            </Avatar>
+            <div>
+              <div className="font-medium">{club.name}</div>
+            </div>
+          </div>
+}
 
-  const latestPost = await api.post.getLatest();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+const FundCard = async ({fund}:{fund: FundRaise & {club: Club}}) => {
+  const raised = await db.donation.aggregate({
+    _sum: {
+      amount: true
+    },
+    where: {
+      fund_raise_id: fund.id
+    }
+  })
 
-      <CreatePost />
-    </div>
-  );
+          return <div className="rounded-lg shadow-xl border-lg bg-white ">
+            <div className="flex items-center gap-2 p-4">
+              <Avatar className="w-8 h-8">
+                <AvatarImage alt="@shadcn" src={'/uploads/' + fund.club.image}/>
+                <AvatarFallback>JP</AvatarFallback>
+              </Avatar>
+              <div className="font-medium">{fund.club.name}</div>
+              <div className="text-gray-500 dark:text-gray-400 text-sm"> • 2h</div>
+            </div>
+            <div>
+              <img
+                alt="Cafe Interior"
+                className="rounded-lg object-cover w-full "
+                height={400}
+                src={`/uploads/${fund.images[0]}`}
+                style={{
+                  aspectRatio: "600/400",
+                  objectFit: "cover",
+                }}
+              />
+              <p className="p-4 text-lg ">{fund.description}</p>
+      <div className="flex px-7 justify-evenly py-5">
+        <div>
+          <div className="text-xl font-semibold">Goal</div>
+          <p>{fund.goal.toFixed(2)}ETB</p>
+        </div>
+
+        <div>
+          <div className="text-xl font-semibold">Raised</div>
+          <p>{raised._sum.amount?.toFixed(2) ?? 0}ETB</p>
+        </div>
+      </div>
+      <div className="mb-6 px-4 flex justify-stretch"><DonateDailog fund={fund}/></div>
+            </div>
+          </div>
+}
+
+const PostCard = ({post}:{post: Post & {club: Club}}) => {
+
+          return <div className="rounded-lg shadow-xl border-lg bg-white ">
+            <div className="flex items-center gap-2 p-4">
+              <Avatar className="w-8 h-8">
+                <AvatarImage alt="@shadcn" src={'/uploads/' + post.club.image}/>
+                <AvatarFallback>JP</AvatarFallback>
+              </Avatar>
+              <div className="font-medium">{post.club.name}</div>
+              <div className="text-gray-500 dark:text-gray-400 text-sm"> • 2h</div>
+            </div>
+            <div>
+              <img
+                alt="Cafe Interior"
+                className="rounded-lg object-cover w-full "
+                height={400}
+                src={`/uploads/${post.images[0]}`}
+                style={{
+                  aspectRatio: "600/400",
+                  objectFit: "cover",
+                }}
+              />
+              <p className="p-4 text-lg ">{post.Caption}</p>
+            </div>
+          </div>
 }
